4f14096e5c8bae94979fe24b8f42a294d303051b
Mike Feldstein <msfeldstein@gmail.com>
2018-04-19 18:42:23 -0700

    Add trails

--- object-preview.js
+++ object-preview.js
@@ -8,6 +8,8 @@ const Target = require('./Target')
 const Room = require('./Room')
 const ControlKit = require('controlkit')
 
+const zero = new THREE.Vector3
+
 const controlValues = {
   triggerValue: 0,
   triggerRange: [0, 1],
@@ -32,31 +34,18 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10)
 scene.add(camera)
 camera.position.z = 2
+camera.position.y = 1
+camera.lookAt(new THREE.Vector3)
 scene.add(new THREE.HemisphereLight(0x606060, 0x404040))
 
-const ball = new Ball()
+
+const ball = new Ball(scene)
 ball.position.x = -1.5
 
-const controller = new Controller()
-const wand = new Wand(controller)
-controller.position.x = 1.5
-gameObjects.push(wand)
-
-scene.add(ball, controller)
-
-const guide = new GuideLine(ball, wand)
-scene.add(guide)
-gameObjects.push(guide)
+scene.add(ball)
+gameObjects.push(ball)
 
 
-const target = new Target()
-scene.add(target)
-gameObjects.push(target)
-target.position.y = 0.5
-
-const room = new Room(1)
-scene.add(room)
-gameObjects.push(room)
 
 function animate() {
   renderer.animate(render)
@@ -64,20 +53,18 @@ function animate() {
 
 function render(at) {
   const t = clock.getElapsedTime()
+  // camera.position.z = Math.sin(t / 4) * 2
+  // camera.position.x = Math.cos(t / 4) * 2
+  ball.position.x = Math.cos(t) * 2
+  ball.position.y = Math.cos(10 + t * 1.1) *2
+  ball.position.z = Math.cos(Math.PI + t * 1.2) - 2
+  camera.lookAt(zero)
   TWEEN.update(at)
-  controller.setTriggerValue(controlValues.triggerValue)
-  controller.setAxes(controlValues.axes)
   gameObjects.forEach(g => g.update(t))
   renderer.render(scene, camera)
 }
 
 window.addEventListener('keydown', _ => {
-  target.trigger()
-  if (room.shattered) {
-    room.reset()
-  } else {
-    room.shatter()
-  }
 })
 
 
